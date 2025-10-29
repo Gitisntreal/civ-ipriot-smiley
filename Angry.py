14e262faaba50b0ad498c64c3727eddd314e8814
Gitisntreal <20134959@tafe.wa.edu.au>
2025-10-29 13:47:28 +0800

    Update Angry.py
    i forgot to save on the actaul thing, transfering the code over ( no wonder he was so angry he had no code)

---
+++ Angry.py
@@ -0,0 +1,30 @@
+from smiley import Smiley
+import time
+
+class Angry(Smiley):
+
+    def __init__(self):
+        # Pass RED as the complexion to the Smiley superclass
+        super().__init__(complexion=self.RED)
+        self.draw_mouth()
+        self.draw_eyes()
+
+    def draw_mouth(self):
+
+        mouth = [49, 54, 42, 43, 44, 45]  
+        for pixel in mouth:
+            self.pixels[pixel] = self.BLANK
+
+    def draw_eyes(self, wide_open=True):
+
+        eyes = [10, 13, 18, 21]
+        for pixel in eyes:
+            self.pixels[pixel] = self.BLANK if wide_open else self.complexion()
+
+    def blink(self, delay=0.25):
+
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        self.show()
