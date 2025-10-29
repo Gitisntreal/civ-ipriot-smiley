6b34e11f8ac6b3dbc79aa629d92b1adac1b78c56
Gitisntreal <20134959@tafe.wa.edu.au>
2025-10-29 13:00:46 +0800

    Update sad.py
    refractor eyes complexion

--- sad.py
+++ sad.py
@@ -23,11 +23,8 @@ class Sad(Smiley):
         """
         eyes = [10, 13, 18, 21]
         for pixel in eyes:
-            if wide_open:
-                eyes = self.BLANK
-            else:
-                eyes = self.YELLOW
-            self.pixels[pixel] = eyes
+            for pixel in eyes:
+                self.pixels[pixel] = self.BLANK if wide_open else self.complexion()
     def blink(self, delay = 0.25):
         self.draw_eyes(wide_open = False)
         self.show()
